3bebf3af9176d64db3485831628dd972e13d1990
stepin104338 <chilukuri.yuvateja@gamil.com>
2021-02-24 14:50:09 +0530

    Create VariableDemo.java

---
+++ Day 1/2. Variable/VariableDemo.java
@@ -0,0 +1,9 @@
+class VariableDemo
+{
+	public static void main(String[] args)
+	{
+		int a = 10; // local variable. 
+		//public int a =10; access specifier is not allowed for local variable. Only final is allowed.
+		System.out.println(a);
+	}
+}
