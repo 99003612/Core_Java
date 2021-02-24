91e4b00b37fd486a7877ca8553b1a93d74c2ef17
stepin104338 <chilukuri.yuvateja@gamil.com>
2021-02-24 14:50:14 +0530

    Create LiteralsDemo.java

---
+++ Day 1/3. Literals/LiteralsDemo.java
@@ -0,0 +1,10 @@
+class LiteralsDemo
+{
+	public static void main(String[] args)
+	{
+		int a = 023;
+		//int a =079 shows error "Integer number too long and 9 is not possible in octal. 
+		//converts into octal and sums and print value of octal(16+3)
+		System.out.println(a);
+	}
+}
