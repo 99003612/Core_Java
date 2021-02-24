5bde699ea5869cde7b83773c6f2a6a29b73008f0
stepin104338 <chilukuri.yuvateja@gamil.com>
2021-02-24 14:49:51 +0530

    Create One.java

---
+++ Day 1/1. Introduction/One.java
@@ -0,0 +1,24 @@
+class One
+{
+	public static void main(String[] args)
+	{
+		System.out.println("My First Program -- Inside Class ONE");
+	}
+
+}
+
+class Two
+{
+	public static void main(String[] args)
+	{
+		System.out.println("My First Program -- Inside Class TWO");
+	}
+}
+
+class Three
+{
+	public static void main(String[] args)
+	{
+		System.out.println("My First Program -- Inside Class THREE");
+	}
+}
