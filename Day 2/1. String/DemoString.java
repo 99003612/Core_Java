60a105862e70bd10097b5484a382c40116a98c37
stepin104338 <chilukuri.yuvateja@gamil.com>
2021-02-25 12:02:45 +0530

    Create DemoString.java

---
+++ Day 2/1. String/DemoString.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+class DemoString
+{
+	public static void main(String[] args)
+	{
+		String s1="Hello"; //String literal
+		String s2= new String("Hello"); //String object and its value
+		System.out.println("Length  of Hello: "+s1.length());
+	}
+}
