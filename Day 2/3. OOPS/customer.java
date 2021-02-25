abe12396af75c7a39fcdf2b788e0f3ee48647ff5
stepin104338 <chilukuri.yuvateja@gamil.com>
2021-02-25 14:53:19 +0530

    Create customer.java

---
+++ Day 2/3. OOPS/customer.java
@@ -0,0 +1,23 @@
+
+public class customer 
+{
+	private int number;
+	private String name;
+	
+	public int getNumber()
+	{
+		return number;
+	}
+	public void setNumber(int num)
+	{
+		number = num;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+}
