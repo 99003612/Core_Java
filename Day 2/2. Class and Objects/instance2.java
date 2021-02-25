99633b2d047aa401f547beb0f98f8aae419dd364
stepin104338 <chilukuri.yuvateja@gamil.com>
2021-02-25 14:53:08 +0530

    Create instance2.java

---
+++ Day 2/2. Class and Objects/instance2.java
@@ -0,0 +1,14 @@
+
+public class instance2 {
+	
+	public static void main(String[] args)
+	{
+		instancemethod i = new instancemethod();
+		i.numbe = 100;
+		i.nam = "Java";
+		i.show();
+		i.displa();
+		i.vision();
+	}
+
+}
