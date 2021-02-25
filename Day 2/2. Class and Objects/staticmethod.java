e48e03bfd1acd90ddd940814317a73e0048e848c
stepin104338 <chilukuri.yuvateja@gamil.com>
2021-02-25 14:53:11 +0530

    Create staticmethod.java

---
+++ Day 2/2. Class and Objects/staticmethod.java
@@ -0,0 +1,19 @@
+
+public class staticmethod {
+
+	public int numbe;
+	public String nam;
+	public static void show()
+	{
+		System.out.println("I am static method");
+	}
+		
+	public void displa()
+	{
+		System.out.println("Reg No "+numbe);
+		System.out.println("Name "+ nam);
+	}
+
+}
+
+
