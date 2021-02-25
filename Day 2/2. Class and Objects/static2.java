32be165ba0d34b7c42fa36e63d7f61c5c22fb368
stepin104338 <chilukuri.yuvateja@gamil.com>
2021-02-25 14:53:13 +0530

    Create static2.java

---
+++ Day 2/2. Class and Objects/static2.java
@@ -0,0 +1,12 @@
+
+public class static2 {
+
+	public static void main(String[] args) {
+		staticmethod s= new staticmethod();
+		s.numbe = 100;
+		s.nam = "Java";
+		s.displa();
+		s.show();
+	}
+
+}
