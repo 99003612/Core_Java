4f7de59dd70f414c0903878e1c6fcf780c7b4832
stepin104338 <chilukuri.yuvateja@gamil.com>
2021-02-25 14:52:59 +0530

    Create class1.java

---
+++ Day 2/2. Class and Objects/class1.java
@@ -0,0 +1,12 @@
+
+public class class1 {
+	public int number;
+	public String name;
+	
+	public void Display()
+	{
+		System.out.println("Reg No "+number);
+		System.out.println("Name "+ name);
+	}
+
+}
