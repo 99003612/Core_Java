d87bad9274301fef9d823397c7bd24fa5a331a46
stepin104338 <chilukuri.yuvateja@gamil.com>
2021-02-25 14:53:22 +0530

    Create customermain.java

---
+++ Day 2/3. OOPS/customermain.java
@@ -0,0 +1,15 @@
+
+public class customermain {
+
+	public static void main(String[] args) 
+	{
+		customer c= new customer();
+		c.setName("Java OOPS");
+		
+		c.setNumber(100);
+		
+		System.out.println(c.getNumber() + c.getName());
+
+	}
+
+}
